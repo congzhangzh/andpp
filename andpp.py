88daaf78141ee48bd6a15a28785cfb78d587588a
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2025-01-21 23:47:25 +0800

    simple it

--- andpp.py
+++ andpp.py
@@ -1,8 +1,7 @@
 import fire
 import os
-from scapy.all import sniff, sendp, Ether, IPv6, ICMPv6ND_NS, ICMPv6ND_NA, ICMPv6NDOptDstLLAddr
+from scapy.all import sniff, sendp, Ether, IPv6, ICMPv6ND_NS, ICMPv6ND_NA, ICMPv6NDOptDstLLAddr, get_if_hwaddr
 
-is_strict_match="STRICT" in os.environ
 ns_counter=0
 def dump_nd_so(x):
     global ns_counter
@@ -22,21 +21,16 @@ def send_nd_ad_for(x, outer_ether_addr):
     sendp(Ether(dst=ether_dst, src=ether_src)/IPv6(version=6, tc=0, fl=0, plen=32, hlim=255, src=x[ICMPv6ND_NS].tgt, dst=x[IPv6].src)/ICMPv6ND_NA(code=0, R=0, S=1, O=1, res=0x0, tgt=x[ICMPv6ND_NS].tgt)/ICMPv6NDOptDstLLAddr(type=2, len=1, lladdr=outer_ether_addr), iface=iface)
     return True
 
-def is_expected_nd_so(x, expected_prefix, router_ether_addr):
-    if is_strict_match:
-        return x[Ether].src==router_ether_addr and x[ICMPv6ND_NS].tgt.startswith(expected_prefix)
-    else:
-        return x[ICMPv6ND_NS].tgt.startswith(expected_prefix)
+def is_expected_nd_so(x, expected_prefix):
+    return x[ICMPv6ND_NS].tgt.startswith(expected_prefix)
 
-def is_expected_nd_ad(x, expected_prefix, router_ether_addr):
-    if is_strict_match:
-        return x[Ether].dst==router_ether_addr and x[ICMPv6ND_NA].tgt.startswith(expected_prefix)
-    else:
-        return x[ICMPv6ND_NA].tgt.startswith(expected_prefix)
+def is_expected_nd_ad(x, expected_prefix):
+    return x[ICMPv6ND_NA].tgt.startswith(expected_prefix)
 
-def main(outer_iface, outer_ether_addr, router_ether_addr, proxy_net_prefix):
+def main(outer_iface, proxy_net_prefix):
+    outer_ether_addr=get_if_hwaddr(outer_iface)
     sniff(filter='icmp6',
-          lfilter= lambda x: is_expected_nd_so(x, proxy_net_prefix, router_ether_addr) if ICMPv6ND_NS in x else is_expected_nd_ad(x, proxy_net_prefix, router_ether_addr) if ICMPv6ND_NA in x else False,
+          lfilter= lambda x: is_expected_nd_so(x, proxy_net_prefix) if ICMPv6ND_NS in x else is_expected_nd_ad(x, proxy_net_prefix) if ICMPv6ND_NA in x else False,
           prn= lambda x: (dump_nd_so(x),send_nd_ad_for(x, outer_ether_addr))[0] if ICMPv6ND_NS in x else dump_nd_ad(x),
           iface=outer_iface)
 
