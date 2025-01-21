7d40facb057b1f6f6c55f60246b1087ecf683e99
Cong Zhang <13283869+congzhangzh@users.noreply.github.com>
2025-01-21 23:17:39 +0800

    easy debug

--- andpp.py
+++ andpp.py
@@ -1,23 +1,35 @@
 import fire
+import os
 from scapy.all import sniff, sendp, Ether, IPv6, ICMPv6ND_NS, ICMPv6ND_NA, ICMPv6NDOptDstLLAddr
 
+is_strict_match="STRICT" in os.environ
+ns_counter=0
 def dump_nd_so(x):
-    return x.sprintf("ether src:%Ether.src%->ether dst:%Ether.dst% ipv6 src:%IPv6.src%->ipv6 dst:%IPv6.dst% ns tgt:%ICMPv6ND_NS.tgt% options-lladdr:%ICMPv6NDOptSrcLLAddr.lladdr%")
+    global ns_counter
+    ns_counter+=1
+    return x.sprintf(rf"NS({ns_counter}) >> ether src:%Ether.src%->ether dst:%Ether.dst% ipv6 src:%IPv6.src%->ipv6 dst:%IPv6.dst% ns tgt:%ICMPv6ND_NS.tgt% options-lladdr:%ICMPv6NDOptSrcLLAddr.lladdr%")
 
+na_counter=0
 def dump_nd_ad(x):
-    return x.sprintf("ether src:%Ether.src%->ether dst:%Ether.dst% ipv6 src:%IPv6.src%->ipv6 dst:%IPv6.dst% na tgt:%ICMPv6ND_NA.tgt% options-lladdr:%ICMPv6NDOptDstLLAddr.lladdr%")
+    global na_counter
+    na_counter+=1
+    return x.sprintf(rf"NA({na_counter}) >> ether src:%Ether.src%->ether dst:%Ether.dst% ipv6 src:%IPv6.src%->ipv6 dst:%IPv6.dst% na tgt:%ICMPv6ND_NA.tgt% options-lladdr:%ICMPv6NDOptDstLLAddr.lladdr%")
 
 def send_nd_ad_for(x, iface, ether_src, ether_dst, outer_ether_addr):    
     sendp(Ether(dst=ether_dst, src=ether_src)/IPv6(version=6, tc=0, fl=0, plen=32, hlim=255, src=x[ICMPv6ND_NS].tgt, dst=x[IPv6].src)/ICMPv6ND_NA(code=0, R=0, S=1, O=1, res=0x0, tgt=x[ICMPv6ND_NS].tgt)/ICMPv6NDOptDstLLAddr(type=2, len=1, lladdr=outer_ether_addr), iface=iface)
     return True
 
 def is_expected_nd_so(x, expected_prefix, router_ether_addr):
-    # return x[Ether].src==router_ether_addr and x[ICMPv6ND_NS].tgt.startswith(expected_prefix)
-    return x[ICMPv6ND_NS].tgt.startswith(expected_prefix)
+    if is_strict_match:
+        return x[Ether].src==router_ether_addr and x[ICMPv6ND_NS].tgt.startswith(expected_prefix)
+    else:
+        return x[ICMPv6ND_NS].tgt.startswith(expected_prefix)
 
 def is_expected_nd_ad(x, expected_prefix, router_ether_addr):
-    # return x[Ether].dst==router_ether_addr and x[ICMPv6ND_NA].tgt.startswith(expected_prefix)
-    return x[ICMPv6ND_NA].tgt.startswith(expected_prefix)
+    if is_strict_match:
+        return x[Ether].dst==router_ether_addr and x[ICMPv6ND_NA].tgt.startswith(expected_prefix)
+    else:
+        return x[ICMPv6ND_NA].tgt.startswith(expected_prefix)
 
 def main(outer_iface, outer_ether_addr, router_ether_addr, proxy_net_prefix):
     sniff(filter='icmp6',
